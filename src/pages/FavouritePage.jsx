fc9212a4ecaf9b324469c45629f30e06c808a7b4
tgorella <107557323+tgorella@users.noreply.github.com>
2023-04-02 16:18:30 +0300

    fix: fav page bugs fix

--- src/pages/FavouritePage.jsx
+++ src/pages/FavouritePage.jsx
@@ -6,6 +6,18 @@ import { useMember } from '../hooks/useMember';
 const FavouritePage = () => {
 	const favourite = JSON.parse(localStorage.getItem("fav-members"))
 	const { members } = useMember();
+
+	if (!favourite || favourite === []) {
+		return (
+			<div className="member-info-wrapper">
+				<BreadCrumbs />
+				<h1>Избранное</h1>
+				<div>
+				<h3>Вы еще никого не добавили в избранное</h3>
+				</div>
+			</div>
+		);
+	}
 const favMembers = favourite.map((i) => {
 	return members.find((el) => el.id === i)
 	})
@@ -14,11 +26,9 @@ const favMembers = favourite.map((i) => {
 			<BreadCrumbs />
       <h1>Избранное</h1>
 			<div>
-			{ !favourite
-			? "Вы еще никого не отметили"
-			: (favMembers.map((m) => {
+			{ favMembers.map((m) => {
 				return (<MemberPreview key={m.id} member={m} />)
-			}))}
+			})}
     </div>
 		</div>
   );
